9a88c150d16d04f47864fb68eabb0ccb60997b43
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-29 21:37:05 +0100

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+
+
+class ProjectController extends Controller 
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::paginate(12);
+        $projects = Project::orderByDesc('id')->paginate(12);
+        $title = "Projects";
+        return view('admin.projects.index', compact('projects', 'title'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $title = 'Create a new project';
+        $types = Type::all();
+        return view('admin.projects.create', compact('title','types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $data = $request->validated();
+        
+        $project = new Project();
+
+        // $project->title = $data['title'];
+        // $project->repo = $data['repo'];
+        // $project->description = $data['description'];
+        
+        $project->fill($data);  
+
+        $project->slug  = Str::slug($project->title);
+        $project->save();
+
+        return redirect()->route('admin.projects.show', $project);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        $title = 'Edit project';
+        $types = Type::all();
+        // $project = Project::findOrFail($id);
+        return view('admin.projects.edit', compact('title', 'project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Project  $project
+     * * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $data = $request->validated();
+        $project->fill($data);
+        $project->slug = Str::slug($project->title);
+        $project->save();
+
+        return redirect()->route('admin.projects.show', $project);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return redirect()->route('admin.projects.index', $project);
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+         $type_ids = Type::all()->pluck('id');
+
+        for ($i = 0; $i < 20; $i++) {
+            $project = new Project();
+            $project->type_id = $faker->randomElement($type_ids);
+            $project->title = $faker->catchPhrase();
+            $project->slug  = Str::slug($project->title);
+            $project->repo = $faker->url();
+            $project->description = $faker->text(50);
+            $project->save();
+        }
+    }
+}
